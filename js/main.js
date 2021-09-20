569626fd9da1d6f470d4a5977cb2fc74c3466e40
myepishkin <myepishkin@exadel.com>
2021-09-21 02:03:58 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-!(w.onload = function() {
-    el = document.getElementById('jjj');
-    setInterval(function() {
-        var today = new Date();
-        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date+' '+time;
-        
-        el.innerHTML = dateTime;
-    }, 1000)
+(function(w) {
+    w.onload = function() {
+        el = document.getElementById('jjj');
+        setInterval(function() {
+            var today = new Date();
+            var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var dateTime = date+' '+time;
+            
+            el.innerHTML = dateTime;
+        }, 1000)
+    }
 })(window)
